b17caa7f95de0c2f979475b2e597f5f8afc84fe5
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-04 09:59:26 -0700

    Create SortArray.java

---
+++ SortArray.java
@@ -0,0 +1,24 @@
+public class SortArray
+{
+	public static void main(String[] args) {
+	    int[] arr = {25,65,34,23,12};
+	    
+	    for(int i=0;i<arr.length-1;i++)
+	    {
+	        for(int j=0;j<arr.length-1-i;j++)
+	        {
+	        if(arr[j]>arr[j+1])
+	        {
+	            int temp=arr[j];
+	            arr[j]=arr[j+1];
+	            arr[j+1]=temp;
+	        }
+	    } 
+	}
+	System.out.println("Sorted array:");
+	for(int i=0;i<arr.length;i++)
+	{
+	    System.out.print(arr[i]+" ");
+	}
+  }
+}
