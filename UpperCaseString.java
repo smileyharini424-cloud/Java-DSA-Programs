2cdccbe4f1c9cdd861bf4233fe61de29938adb4b
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-05 03:21:48 -0700

    Create UpperCaseString.java

---
+++ UpperCaseString.java
@@ -0,0 +1,9 @@
+public class UpperCaseString
+{
+    public static void main(String[] args)
+    {
+        String str = "Ramya";
+        
+        System.out.println("Uppercase: " + str.toUpperCase());
+    }
+}
