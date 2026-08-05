219cb569ba98bc547122706c85835a86d737673c
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-05 03:14:49 -0700

    Create CountCharacters.java

---
+++ CountCharacters.java
@@ -0,0 +1,8 @@
+public class CountCharacters
+{
+    public static void main(String[] args)
+    {
+        String str = "Reshu";
+        System.out.println("Total characters: " + str.length());
+    }
+}
