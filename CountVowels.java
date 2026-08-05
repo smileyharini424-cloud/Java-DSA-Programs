b6ab6adde468369a1fdc6a84b9a646f765574579
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-05 03:10:46 -0700

    Create CountVowels.java

---
+++ CountVowels.java
@@ -0,0 +1,18 @@
+public class CountVowels
+{
+    public static void main(String[] args)
+    {
+        String str = "Rama";
+        int count = 0;
+        for(int i=0;i<str.length();i++)
+        {
+            char ch=Character.toLowerCase(str.charAt(i));
+            
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+            {
+                count++;
+            }
+        }
+        System.out.println("Number of vowels: " + count);
+    }
+}
