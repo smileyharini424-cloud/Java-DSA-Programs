18c5506e392f32b9f35576e87472ea71321c0a32
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-08 23:31:27 -0700

    Create FibonacciSeries.java

---
+++ FibonacciSeries.java
@@ -0,0 +1,17 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 7;
+        int first = 0;
+        int second = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
