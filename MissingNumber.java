deb6c26d008bc97554bdf8a40a497edcc34cec06
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-08 23:14:31 -0700

    Create MissingNumber.java

---
+++ MissingNumber.java
@@ -0,0 +1,19 @@
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5};
+
+        int n = 5;
+
+        int total = n * (n + 1) / 2;
+
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        int missing = total - sum;
+
+        System.out.println("Missing Number : " + missing);
+    }
+}
