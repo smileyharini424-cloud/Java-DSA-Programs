a12c4c91164b5330433d9ed9a5dc14fb32a0c433
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-10 05:56:51 -0700

    Create PowerOfNumber.java

---
+++ PowerOfNumber.java
@@ -0,0 +1,13 @@
+public class PowerOfNumber
+{
+	public static void main(String[] args) {
+	    int base = 2;
+	    int exponent = 5;
+	    int result = 1;
+	    for(int i=1;i<=exponent;i++)
+	    {
+	        result *= base;
+	    }
+		System.out.println(base + " raised to " + exponent + " = " + result);
+	}
+}
