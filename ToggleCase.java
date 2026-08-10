e52774af9df35b5dccffb81652da139f0d9c2345
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-10 06:20:13 -0700

    Create ToggleCase.java

---
+++ ToggleCase.java
@@ -0,0 +1,21 @@
+public class ToggleCase {
+    public static void main(String[] args) {
+        String str = "JaVa PrOgRaMmInG";
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (Character.isUpperCase(ch))
+                result += Character.toLowerCase(ch);
+            else if (Character.isLowerCase(ch))
+                result += Character.toUpperCase(ch);
+            else
+                result += ch;
+        }
+
+        System.out.println("Original: " + str);
+        System.out.println("Toggle Case: " + result);
+    }
+}
