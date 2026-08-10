06655ac103e03e116fa87a9cc2962244e1f199e8
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-09 19:58:07 -0700

    Create LCM.java

---
+++ LCM.java
@@ -0,0 +1,21 @@
+public class LCM
+{
+	public static void main(String[] args) {
+	    int a = 12;
+	    int b = 18;
+	    
+	    int x = a;
+	    int y = b;
+	    
+	    while(y != 0)
+	    {
+	        int temp = y;
+	        y = x % y;
+	        x = temp;
+	    }
+	    int gcd = x;
+	    int lcm = (a * b) / gcd;
+	    System.out.println("LCM = " + lcm);
+	    
+	   }
+	}
